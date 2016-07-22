f0677a03f510fd49d5db38421049e7a29725c654
Marco Carrizales <a00512098@itesm.mx>
2016-07-21 22:13:27 -0500

    Problem 0009 incomplete
    Stuck on this problem.
    I don't even know how to face this one.

---
+++ 0009.js
@@ -0,0 +1,38 @@
+var a = 1;
+var b = 2;
+var c = 997;
+var product = 0;
+var sum;
+var bSqr;
+var cSqr;
+
+while (c > b) {
+  bSqr = pow(b, 2);
+  cSqr = pow(c, 2);
+  while (b > a) {
+    sum = pow(a, 2) + bSqr;
+    if (sum >)
+  }
+}
+
+document.getElementById('answer').innerHTML = "Answer: " + product;
+console.log("Answer: "  + product);
+
+// ------------------- functions part --------------------------
+
+// Returns true if the number is an Integer value.
+function isInt(num) {
+  return num % 1 === 0;
+}
+
+//               **WARNING**
+// This function only works for integer numbers. :(
+// Do not use it with floating point numbers.
+// Anyway I'm leaving it here, you don't know when
+// you can need your own pow(a,b) function.
+function pow(base, exponent) {
+  if(exponent == 0)
+    return 1;
+
+  return base * pow(base, exponent - 1);
+}
